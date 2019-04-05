02e5e95633df723d501e4ea35339149850034e9c
hqZing <huangqii@live.com>
2019-04-05 18:14:00 +0800

    找到了解决Request对象数据残留问题的方式

--- http_client.py
+++ http_client.py
@@ -2,9 +2,6 @@ import socket
 import re
 import dns.resolver
 
-import requests
-import urllib3
-
 
 class StatusLine:
     """
@@ -30,7 +27,7 @@ class Utils:
     这个函数是将URI解析为请求行的URI，顺便还将主机名和端口分析出来
     """
     @classmethod
-    def parse_uri(cls, uri):
+    def parse_uri(cls, uri, **kwargs):
         rtn_dict = {
             'port': 80,
             'request_uri': '/',
@@ -107,9 +104,9 @@ class Utils:
         return rtn_dict
 
 
-a = b'HTTP/1.1 200 OK\r\nAccess-Control-Allow-Credentials: true\r\nAccess-Control-Allow-Origin: *\r\nContent-Type: application/json\r\nDate: Thu, 04 Apr 2019 12:34:15 GMT\r\nServer: nginx\r\nContent-Length: 178\r\nConnection: Close\r\n\r\n{\n  "args": {\n    "id": "1"\n  }, \n  "headers": {\n    "Host": "www.httpbin.org"\n  }, \n  "origin": "111.59.124.142, 111.59.124.142", \n  "url": "https://www.httpbin.org/get?id=1"\n}\n'
+# a = b'HTTP/1.1 200 OK\r\nAccess-Control-Allow-Credentials: true\r\nAccess-Control-Allow-Origin: *\r\nContent-Type: application/json\r\nDate: Thu, 04 Apr 2019 12:34:15 GMT\r\nServer: nginx\r\nContent-Length: 178\r\nConnection: Close\r\n\r\n{\n  "args": {\n    "id": "1"\n  }, \n  "headers": {\n    "Host": "www.httpbin.org"\n  }, \n  "origin": "111.59.124.142, 111.59.124.142", \n  "url": "https://www.httpbin.org/get?id=1"\n}\n'
 
-b = Utils.parse_response(a)
+# b = Utils.parse_response(a)
 
 # print(b)
 # b = a.split(b"\r\n")
@@ -127,13 +124,88 @@ class RequestLine:
     Request-Line   = Method SP Request-URI SP HTTP-Version CRLF
     """
 
-    def __init__(self, method, request_uri):
-        self.method = method.upper()                                          # 请求方法，get/post/put/delete等
-        self.request_uri = request_uri                                        # 请求URI
-        self.http_version = "HTTP/1.1"                                      # HTTP版本 ，这里默认只使用1.1
+    def __init__(self):
+        self.__method = "GET"                                      # 请求方法，get/post/put/delete等
+        self.__request_uri = "/"                                        # 请求URI
+        self.__http_version = "HTTP/1.1"                                      # HTTP版本 ，这里默认只使用1.1
 
-    def to_text(self):
-        return self.method + " " + self.request_uri + " " + self.http_version + "\r\n"
+    def get_method(self):
+        return self.__method
+
+    def get_request_uri(self):
+        return self.__request_uri
+
+    def get_http_version(self):
+        return self.__http_version
+
+    def set_method(self, method):
+        self.__method = method
+
+    def set_request_uri(self, request_uri):
+        self.__request_uri = request_uri
+
+    def set_http_version(self, http_version):
+        self.__http_version = http_version
+
+    def get_byte(self):
+        return (self.__method + " " + self.__request_uri + " " + self.__http_version + "\r\n").encode()
+
+
+class Headers:
+    def __init__(self):
+        self.__headers = {}
+
+    def set_headers(self, headers):
+        self.__headers.update(headers)
+
+    def get_headers(self):
+        return self.__headers
+
+    def get_byte(self):
+        rtn_text = ""
+        for k, v in self.__headers.items():
+            rtn_text += k + ': ' + str(v) + '\r\n'
+        return rtn_text.encode()
+
+
+class Body:
+    def __init__(self):
+        self.__charset = "utf-8"
+        self.__content_length = 0           # 消息长度，计算方法依据 RFC2616 Section 4.4，
+        self.__content = ""
+
+        # 这里把一部分实体首部给放进来比较合适，方便编程，发送报文的时候会将这里的内容更新到request对象的Headers里面
+        self.__part_header = None
+
+    def update(self):
+        # 每次切换字符集之后执行一下这个，确保主体长度是正确的数值
+        temp = self.__content.encode(self.__charset)
+        self.__content_length = temp.__len__()
+        return temp
+
+    # 需要定义set和get方法是因为不想让用户能直接对这些属性进行操作，
+    # 因为每个操作要额外执行一步update操作，直接操作属性的话会导致内容、字符集、长度不匹配
+    def set_content(self, content):
+        self.__content = content
+        self.update()
+
+    def set_charset(self, charset):
+        self.__charset = charset
+        self.update()
+
+    def get_content(self):
+        return self.__content
+
+    def get_content_length(self):
+        return self.__content_length
+
+    def get_charset(self):
+        return self.__charset
+
+    def get_byte(self):
+        # 目前就不进行压缩编码了，统一用utf-8编码。得出来是多少就是多少，不进行压缩
+
+        return self.update()
 
 #
 # r = RequestLine("get", "http://www.baidu.com/get")
@@ -156,61 +228,80 @@ class Request:
 
     def __init__(self):
 
-        self.__request_line = None               # 请求行
-        self.__request_header = {}               # 请求首部
-        self.__general_header = {}               # 通用首部
-        self.__entity_header = {}                # 实体首部
-        self.__extend_header = {}                # 扩展首部
-        self.__body = None                       # 报文主体
+        self.__request_line = RequestLine()               # 请求行
+        self.__headers = Headers()                 # 为了与ressponse对象一致，请求首部\通用首部\实体首部\扩展首部全部混在一起了，不再单独区分
+        self.__body = Body()                       # 报文主体
 
-        self.__infos = None                      # 这个成员变量与HTTP协议无关，方便编程而加入的变量
+        # 下面的变量与HTTP请求报文格式无关，是方便编程而加入的变量
+        self.__infos = None
 
-    def __combine_and_to_bytes(self):
+
+    def __clear(self):
+        # 每次发完一个请求之后，应该执行一次这个代码，将其初始化一遍，除了cookie等必须保存的东西，其他信息应该清除，以免请求携带了上一次的信息
+        return None
+
+    def __create(self, uri, **kwargs):
+
+        self.__infos = Utils.parse_uri(uri)
+        self.__headers.set_headers({"Host": self.__infos["host"], "Connection": "close"})       # 主机名加入请求头
+        self.__request_line.set_request_uri(self.__infos["request_uri"])
+
+        # 请求方法
+        if "method" in kwargs:
+            self.__request_line.set_method(kwargs["method"].upper())
+
+        # get请求的时候可以自动加入这些参数
+        if "params" in kwargs:
+            u = self.__request_line.get_request_uri()
+            if "?" not in u:
+                u += "?"
+                temp = list(kwargs["params"].keys())[0]
+                u += "%s=%s" % (temp, kwargs["params"][temp])
+                kwargs["params"].pop(temp)
+
+            for k, v in kwargs["params"].items():
+                    u += "&%s=%s" % (k, v)
+            self.__request_line.set_request_uri(u)
+
+        # post请求的时候会将这些东西加入报文主体
+        if "data" in kwargs:
+            temp2 = ""
+            temp = list(kwargs["data"].keys())[0]
+            temp2 += "%s=%s" % (temp, kwargs["data"][temp])
+
+            kwargs["data"].pop(temp)
+
+            for k, v in kwargs["data"].items():
+                    temp2 += "&%s=%s" % (k, v)
+
+            self.__body.set_content(temp2)
+
+            # 带上实体首部，指定实体的长度是多少，否则服务端将不能正确识别
+            self.__headers.set_headers({"Content-Length": self.__body.get_content_length()})
+
+    def __combine(self):
 
         # 在这种场景下，不考虑多种MIME类型的问题同时传输和数据分块的问题。在这之后可以再针对这个东西进行优化
+        data_send = b''
+        data_send += self.__request_line.get_byte()                 # 加入起始行
+        data_send += self.__headers.get_byte()              # 拼接报文首部
+        data_send += b'\r\n'                                 # 添加空行
+        data_send += self.__body.get_byte()                 # 添加主体
 
-        data_send = ''
-
-        # 加入起始行
-        data_send += self.__request_line.to_text()
-
-        # 遍历报文首部字典，添加报文首部
-        for k, v in self.__request_header.items():
-            data_send += k + ': ' + v + '\r\n'
-        for k, v in self.__general_header.items():
-            data_send += k + ': ' + v + '\r\n'
-        for k, v in self.__entity_header.items():
-            data_send += k + ': ' + v + '\r\n'
-        for k, v in self.__extend_header.items():
-            data_send += k + ': ' + v + '\r\n'
-
-        # 添加空行
-        data_send += '\r\n'
-
-        # 添加主体
-        if self.__body is not None:
-            data_send += self.__body
-
-        # 这里就先统一以utf-8字符集来编码，回头再讨论在首部字段中指定了实体编码的情况
-        return data_send.encode('utf-8')
-
-    def __get_info_from_uri(self, uri):
-        self.__infos = Utils.parse_uri(uri)
-
-        self.__request_header.update({"Host": self.__infos["host"], "Connection": "close"})       # 主机名加入请求头
+        return data_send
 
     def __send(self, data_send):
         print(data_send)
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect((self.__infos["ip"], self.__infos["port"]))
+        so = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        so.connect((self.__infos["ip"], self.__infos["port"]))
 
-        s.send(data_send)
+        so.send(data_send)
 
-        # 直接循环接收也没啥问题，但是一定要关闭持久连接。在持久连接的情况下，这个循环会堵塞，不停地接收数据
+        # 直接死循环接收也没啥问题，但是一定要关闭持久连接。在持久连接的情况下，这个循环会堵塞，不停地接收数据
         buffer = []
         while True:
             # 每次最多接收1k字节:
-            d = s.recv(1024)
+            d = so.recv(1024)
             if d:
                 buffer.append(d)
                 print(d)
@@ -218,19 +309,29 @@ class Request:
                 break
         data = b''.join(buffer)
 
-
-        s.close()
+        so.close()
 
         return data
 
-    def get(self, uri):
-        self.__get_info_from_uri(uri)
-        self.__request_line = RequestLine("get", self.__infos["request_uri"])
-        response = self.__send(self.__combine_and_to_bytes())
+    def get(self, uri, **kwargs):
+        if "parmas" in kwargs:
+            self.__create(uri, method="get", params=kwargs["parmas"])
+        else:
+            self.__create(uri, method="get")
+        response = self.__send(self.__combine())
         return response
 
-    def post(self):
-        pass
+    def post(self, uri, **kwargs):
+        """
+        在post这里需要注意的地方就是，因为post请求是有body的，需要获取一次body的字节长度加入请求头
+        """
+        if "data" in kwargs:
+            self.__create(uri, method="post", data=kwargs["data"])
+        else:
+            self.__create(uri, method="post")
+
+        response = self.__send(self.__combine())
+        return response
 
     def put(self):
         pass
@@ -239,6 +340,20 @@ class Request:
         pass
 
 
-# s = Request()
-# resp = s.get("http://www.httpbin.org/get?id=1")
-# print(resp)
+s = Request()
+resp = s.post("http://www.httpbin.org/post", data={"b": 1})
+print(resp)
+
+
+class Session:
+    """
+    Session对象是持续不变的，而Request对象是每一次发包都重新创建的
+    每一次都需要重新创建Request对象，是因为残留的变量实在太多了，一不小心可能会将上一次请求报文的内容又给继续带上去
+    """
+    def __init__(self):
+        self.__socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)       # 将套接字储存，用于持久连接和非持久连接
+        self.__headers = None                   # 将报文首部保存，记录客户端状态
+        self.__request = None                      # 真正用来首发请求报文的是这个，这玩意每次都需要创建新的，用完即丢
+
+    def __send(self):
+        pass
