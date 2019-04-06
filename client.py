9cbebf7b0f47f90843d933622f91c3d09a3891ed
hqZing <huangqii@live.com>
2019-04-06 18:00:53 +0800

    进行了一些优雅的修改

--- client.py
+++ client.py
@@ -1,14 +1,19 @@
-import socket
-ip_port = ('127.0.0.1', 9999)
+# import socket
+# ip_port = ('127.0.0.1', 9999)
+#
+# sk = socket.socket()
+# sk.connect(ip_port)
+#
+# sk.sendall('请求占领地球'.encode("utf-8"))
+#
+#
+# server_reply = sk.recv(1024)
+# print(server_reply.decode("utf-8"))
+#
+# sk.close()
 
-sk = socket.socket()
-sk.connect(ip_port)
 
-sk.sendall('请求占领地球'.encode("utf-8"))
+def testt(**kwargs):
+    print(kwargs)
 
-
-server_reply = sk.recv(1024)
-print(server_reply.decode("utf-8"))
-
-sk.close()
-
+testt(data=111)
--- http_client.py
+++ http_client.py
@@ -3,6 +3,21 @@ import re
 import dns.resolver
 
 
+# DNS在本地缓存，不再每次都重复获取
+class DNS:
+    dns_dict = {}
+
+    @classmethod
+    def get_dns(cls, host_string):
+
+        if host_string in DNS.dns_dict:
+            return DNS.dns_dict[host_string]
+        else:
+            ip = dns.resolver.query(host_string, 'A').response.answer[-1].items[0].address
+            DNS.dns_dict.update({host_string: ip})
+            return ip
+
+
 class RequestLine:
 
     """
@@ -12,31 +27,31 @@ class RequestLine:
     """
 
     def __init__(self):
-        self.__method = "GET"                                      # 请求方法，get/post/put/delete等
-        self.__request_uri = "/"                                        # 请求URI
-        self.__http_version = "HTTP/1.1"                                      # HTTP版本 ，这里默认只使用1.1
+        self.method = "GET"                                      # 请求方法，get/post/put/delete等
+        self.request_uri = "/"                                        # 请求URI
+        self.http_version = "HTTP/1.1"                                      # HTTP版本 ，这里默认只使用1.1
 
-    def get_method(self):
-        return self.__method
+    def build(self, method, request_uri, http_version="HTTP/1.1"):
+        """
+        接收参数构建请求行
+        :param method:
+        :param request_uri:
+        :param http_version:
+        :return:
+        """
+        self.method = method
+        self.request_uri = request_uri
+        self.http_version = http_version
 
-    def get_request_uri(self):
-        return self.__request_uri
+    def parse(self, bytes_data):
+        """
+        从自己数据解析成请求行对象，与build作用相反
+        :param bytes_data:
+        :return:
+        """
 
-    def get_http_version(self):
-        return self.__http_version
-
-    def set_method(self, method):
-        self.__method = method
-
-    def set_request_uri(self, request_uri):
-        self.__request_uri = request_uri
-
-    def set_http_version(self, http_version):
-        self.__http_version = http_version
-
-    def get_byte(self):
-        return (self.__method + " " + self.__request_uri + " " + self.__http_version + "\r\n").encode()
-
+    def bytes(self):
+        return (self.method + " " + self.request_uri + " " + self.http_version + "\r\n").encode()
 
 
 class StatusLine:
@@ -51,27 +66,22 @@ class StatusLine:
         self.status_code = ""        # 状态码
         self.reason_phrase = ""      # 原因短语
 
-    def parse(self, string_data):
-        self.http_version = string_data.split(" ")[0]       # http版本
-        self.status_code = string_data.split(" ")[1]        # 状态码
-        self.reason_phrase = string_data.split(" ")[2]      # 原因短语
+    def parse(self, bytes_data):
+        temp = bytes_data.decode().split(" ")
+        self.http_version = temp[0]       # http版本
+        self.status_code = temp[1]        # 状态码
+        self.reason_phrase = temp[2]      # 原因短语
 
 
 class Headers:
     def __init__(self):
-        self.__headers = {}
+        self.headers = {}
 
-    def set_headers(self, headers):
-        self.__headers.update(headers)
+    def parse(self, bytes_data):
+        self.headers = dict([x.split(": ") for x in bytes_data.decode().split("\r\n")])
 
-    def get_headers(self):
-        return self.__headers
-
-    def get_byte(self):
-        rtn_text = ""
-        for k, v in self.__headers.items():
-            rtn_text += k + ': ' + str(v) + '\r\n'
-        return rtn_text.encode()
+    def bytes(self):
+        return "".join([k + ': ' + str(v) + '\r\n' for k, v in self.headers.items()]).encode()
 
 
 class Body:
@@ -108,7 +118,7 @@ class Body:
     def get_charset(self):
         return self.__charset
 
-    def get_byte(self):
+    def bytes(self):
         # 目前就不进行压缩编码了，统一用utf-8编码。得出来是多少就是多少，不进行压缩
 
         return self.update()
@@ -127,63 +137,15 @@ class Response:
     """
 
     def __init__(self):
-        self.__status_line = StatusLine()
-        self.__headers = Headers()
-        self.__body = Body()
-
-    def parse_head(self, bytes_data):
-
-        # 首先分割字节流，找到空行，即\r\n\r\n的地方，对这部分头部进行解码
-        # 这里用什么字符集解码问题都不大，因为首部数据全都是ASCII字符，所以用UTF-8解码是不会出现问题的，
-        # 但是报文主体就不能自己随便指定字符集了，要看头部指明的字符集来进行解码
-
-        # 这个地方因为从服务器返回来的时候不一定按序，所以首部字段不区分类型了，除了状态行以外，其他统一称为headers
-
-        temp = bytes_data.split(b"\r\n\r\n")[0].decode()            # 忽略报文主体，先取出首部字段
-
-        self.__status_line.parse(temp.split("\r\n")[0])      # 在首部信息里面，首先解析状态行
-
-        temp = temp.split("\r\n")[1:]
-        for each in temp:
-            self.__headers.set_headers({each.split(": ")[0]: each.split(": ")[1]})
-
-        print(self.__headers.get_headers())
+        self.status_line = StatusLine()
+        self.headers = Headers()
+        self.body = Body()
 
     def parse_body(self, charset, bytes_data):
-        self.__body.set_content(bytes_data.decode(charset))
-
-    def get_headers(self):
-        return self.__headers.get_headers()
-
-    def set_headers(self, dict_data):
-        self.__headers.set_headers(dict_data)
+        self.body.set_content(bytes_data.decode(charset))
 
     def get_content(self):
-        return self.__body.get_content()
-
-
-
-
-
-
-# a = b'HTTP/1.1 200 OK\r\nAccess-Control-Allow-Credentials: true\r\nAccess-Control-Allow-Origin: *\r\nContent-Type: application/json\r\nDate: Thu, 04 Apr 2019 12:34:15 GMT\r\nServer: nginx\r\nContent-Length: 178\r\nConnection: Close\r\n\r\n{\n  "args": {\n    "id": "1"\n  }, \n  "headers": {\n    "Host": "www.httpbin.org"\n  }, \n  "origin": "111.59.124.142, 111.59.124.142", \n  "url": "https://www.httpbin.org/get?id=1"\n}\n'
-
-# b = Utils.parse_response(a)
-
-# print(b)
-# b = a.split(b"\r\n")
-
-# for each in b:
-#    print(each)
-
-
-
-
-
-#
-# r = RequestLine("get", "http://www.baidu.com/get")
-# print(r.to_text())
-
+        return self.body.get_content()
 
 
 class Request:
@@ -201,62 +163,57 @@ class Request:
 
     def __init__(self):
 
-        self.__request_line = RequestLine()               # 请求行
-        self.__headers = Headers()                 # 为了与ressponse对象一致，请求首部\通用首部\实体首部\扩展首部全部混在一起了，不再单独区分
-        self.__body = Body()                       # 报文主体
+        self.request_line = RequestLine()               # 请求行
+        self.headers = Headers()                 # 为了与ressponse对象一致，请求首部\通用首部\实体首部\扩展首部全部混在一起了，不再单独区分
+        self.body = Body()                       # 报文主体
 
         # 下面的变量与HTTP请求报文格式无关，是方便编程而加入的变量
-        self.__infos = None
-
-    """
-    这个函数是将URI解析为请求行的URI，顺便还将主机名和端口分析出来
-    """
-    def parse_uri(self, uri, **kwargs):
-        rtn_dict = {
+        self.conn_info = {
             'port': 80,
             'request_uri': '/',
             'host': '',
-            'protocal': 'HTTP/1.1',
+            'protocol': 'HTTP/1.1',
             'ip': ''
         }
+
+    def __parse_uri(self, uri):
+        """
+        这个函数是将URI解析为请求行的URI，顺便还将主机名和端口分析出来
+        # 将ip，端口等信息存起来
+        :param uri:
+        :return:
+        """
 
         # 这几个正则表达式应该可以合并成一个的，目前水平不够，先用判断语句来实现
 
         # 解析主机名
         temp = re.match("[^:]+://([^/:]+)", uri)
-        rtn_dict["host"] = temp.group(1)
+        self.conn_info["host"] = temp.group(1)
 
         # 解析端口
         temp = re.match("[^:]+://[^/:]+:([0-9]+)", uri)
         if temp is not None:
-            rtn_dict["port"] = temp.group(1)
+            self.conn_info["port"] = int(temp.group(1))
 
         # 解析request_uri
         temp = re.match("[^:]+://[^/]+/(.+)", uri)
         if temp is not None:
-            rtn_dict["request_uri"] += temp.group(1)
+            self.conn_info["request_uri"] += temp.group(1)
 
         # 使用DNS解析域名得到ip
-        rtn_dict["ip"] = dns.resolver.query(rtn_dict["host"], 'A').response.answer[-1].items[0].address
+        self.conn_info["ip"] = DNS.get_dns(self.conn_info["host"])
 
-        print(rtn_dict)
-        return rtn_dict
+    def build(self, method, uri, **kwargs):
+        """
+        此处根据输入的参数构建请求包
+        """
 
+        self.__parse_uri(uri)
+        self.headers.headers.update({"Host": self.conn_info["host"]})       # 往请求头中加入主机名
 
-
-    def __create(self, uri, **kwargs):
-
-        self.__infos = self.parse_uri(uri)
-        self.__headers.set_headers({"Host": self.__infos["host"], "Connection": "close"})       # 主机名加入请求头
-        self.__request_line.set_request_uri(self.__infos["request_uri"])
-
-        # 请求方法
-        if "method" in kwargs:
-            self.__request_line.set_method(kwargs["method"].upper())
-
-        # get请求的时候可以自动加入这些参数
+        # get请求的参数
+        u = self.conn_info["request_uri"]
         if "params" in kwargs:
-            u = self.__request_line.get_request_uri()
             if "?" not in u:
                 u += "?"
                 temp = list(kwargs["params"].keys())[0]
@@ -265,9 +222,11 @@ class Request:
 
             for k, v in kwargs["params"].items():
                     u += "&%s=%s" % (k, v)
-            self.__request_line.set_request_uri(u)
 
-        # post请求的时候会将这些东西加入报文主体
+        # 构建请求行
+        self.request_line.build(method.upper(), u)
+
+        # post请求的主体数据
         if "data" in kwargs:
             temp2 = ""
             temp = list(kwargs["data"].keys())[0]
@@ -278,75 +237,22 @@ class Request:
             for k, v in kwargs["data"].items():
                     temp2 += "&%s=%s" % (k, v)
 
-            self.__body.set_content(temp2)
+            self.body.set_content(temp2)
 
             # 带上实体首部，指定实体的长度是多少，否则服务端将不能正确识别
-            self.__headers.set_headers({"Content-Length": self.__body.get_content_length()})
+            self.headers.headers.update({"Content-Length": self.body.get_content_length()})
 
-    def __combine(self):
+            print(self.conn_info)
+
+    def bytes(self):
 
         # 在这种场景下，不考虑多种MIME类型的问题同时传输和数据分块的问题。在这之后可以再针对这个东西进行优化
-        data_send = b''
-        data_send += self.__request_line.get_byte()                 # 加入起始行
-        data_send += self.__headers.get_byte()              # 拼接报文首部
-        data_send += b'\r\n'                                 # 添加空行
-        data_send += self.__body.get_byte()                 # 添加主体
-
-        return data_send
-
-    def __send(self, data_send):
-        print(data_send)
-        so = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        so.connect((self.__infos["ip"], self.__infos["port"]))
-
-        so.send(data_send)
-
-        # 直接死循环接收也没啥问题，但是一定要关闭持久连接。在持久连接的情况下，这个循环会堵塞，不停地接收数据
-        buffer = []
-        while True:
-            # 每次最多接收1k字节:
-            d = so.recv(1024)
-            if d:
-                buffer.append(d)
-                print(d)
-            else:
-                break
-        data = b''.join(buffer)
-
-        so.close()
-
-        return data
-
-    def get(self, uri, **kwargs):
-        if "parmas" in kwargs:
-            self.__create(uri, method="get", params=kwargs["parmas"])
-        else:
-            self.__create(uri, method="get")
-        response = self.__send(self.__combine())
-        return response
-
-    def post(self, uri, **kwargs):
-        """
-        在post这里需要注意的地方就是，因为post请求是有body的，需要获取一次body的字节长度加入请求头
-        """
-        if "data" in kwargs:
-            self.__create(uri, method="post", data=kwargs["data"])
-        else:
-            self.__create(uri, method="post")
-
-        response = self.__send(self.__combine())
-        return response
-
-    def put(self):
-        pass
-
-    def delete(self):
-        pass
-
-
-# s = Request()
-# resp = s.post("http://www.httpbin.org/post", data={"b": 1})
-# print(resp)
+        rtn_bytes = b''
+        rtn_bytes += self.request_line.bytes()                   # 加入起始行
+        rtn_bytes += self.headers.bytes()                        # 拼接报文首部
+        rtn_bytes += b'\r\n'                                         # 添加空行
+        rtn_bytes += self.body.bytes()                           # 添加主体
+        return rtn_bytes
 
 
 class Session:
@@ -356,26 +262,44 @@ class Session:
     并且为了更贴切地表示每一次发送的请求报文都是不同的个体，每条请求报文各占用一个对象，这是比较合适的
     """
     def __init__(self):
-        self.__socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)       # 将套接字储存，用于持久连接和非持久连接
-        self.__headers = None                   # 将报文首部保存，记录客户端状态
-        self.__request = Request()                      # 真正用来首发请求报文的是这个，这玩意每次都需要创建新的，用完即丢
+        self.socket = None                              # 将套接字储存，用于持久连接和非持久连接
+        self.store_headers = None                       # 将报文首部保存，记录客户端状态
+        # self.request = Request()                      # 真正用来首发请求报文的是这个，这玩意每次都需要创建新的，用完即丢
 
-        self.__keep_alive = True                        # 持久连接默认都是开启的
+        self.last_request = None                      # 将上一个请求包保存下来，这样，在持久连接的问题就很好处理
+        self.last_response = None
 
-    def send(self, ip, port, bytes_data):
+    def send(self, request):
+        ip, port, bytes_data = request.conn_info["ip"], request.conn_info["port"], request.bytes()
+        print(ip)
+        print(bytes_data)
+
+        flag = False
+
+        # 根据条件, 判断是否复用套接字
+        if self.last_response is None:
+            flag = True
+        elif "close".upper() in self.last_response.headers.headers["Connection"].upper():
+            flag = True
+        elif ip != self.last_request.conn_info["ip"] or port != self.last_request.conn_info["port"]:
+            flag = True
+
+        if flag:
+            # 直接创建新的套接字，旧的那个不关闭了，有时间再回来补全关闭套接字的代码，目前这个不影响程序
+            self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.socket.connect((ip, port))
+
+        self.socket.send(bytes_data)
 
         # 创建Response对象用来存储返回的这些数据
         response = Response()
-
-        self.__socket.connect((ip, port))
-        self.__socket.send(bytes_data)
 
         # 直接死循环接收也没啥问题，但是一定要关闭持久连接。在持久连接的情况下，这个循环会堵塞，不停地接收数据
         buffer = b''
 
         while True:
 
-            d = self.__socket.recv(1024)
+            d = self.socket.recv(1024)
             if d:
                 buffer += d
                 print(buffer)
@@ -385,46 +309,62 @@ class Session:
                 先处理一下读取Content-Length才能依据这个长度来读取报文主体
                 """
                 if b'\r\n\r\n' in buffer:
-                    response.parse_head(buffer)     # 解析响应头
+
+                    temp = buffer[: buffer.find(b"\r\n")]
+                    response.status_line.parse(temp)    # 解析状态行
+
+                    temp = buffer[buffer.find(b"\r\n")+2: buffer.find(b"\r\n\r\n")]
+                    response.headers.parse(temp)  # 解析响应头
                     break
             else:
                 break
 
-        if "Connection" in response.get_headers().keys():
-            if "close" in response.get_headers()["Connection"]:
-                self.__keep_alive = False
-            if "Keep-Alive" in response.get_headers()["Connection"]:
-                self.__keep_alive = True
-
         # 在这里继续接收主体（如果有主体的话）
-        if "Content-Length" in response.get_headers().keys():
+        if "Content-Length" in response.headers.headers.keys():
             print(111)
-            buffer = buffer.split(b"\r\n\r\n")[1]
+            buffer = buffer[buffer.find(b"\r\n\r\n")+4:]
 
             while True:
 
-                if buffer.__len__() >= int(response.get_headers()["Content-Length"]):
+                if buffer.__len__() >= int(response.headers.headers["Content-Length"]):
                     print(222)
                     response.parse_body("utf-8", buffer)
                     break
 
-                d = self.__socket.recv(1024)
+                d = self.socket.recv(1024)
                 if d:
                     buffer += d
                     print(buffer)
                 else:
                     break
 
-        if not self.__keep_alive:
-            self.__socket.close()
         print(333)
         # 持久连接搞定了！！！
-        print(response.get_headers())
+        print(response.status_line.__dict__)
+        print(response.headers.headers)
         print(response.get_content())
+        self.last_request = request
+        self.last_response = response
         return response
+
+    def get(self, uri, **kwargs):
+        request = Request()
+        request.build("GET", uri)
+        self.send(request)
+
+    def post(self, uri, **kwargs):
+        request = Request()
+        request.build("POST", uri)
+        self.send(request)
+
+    def put(self):
+        pass
+
+    def delete(self):
+        pass
 
 
 s = Session()
-ddd = b'POST /post HTTP/1.1\r\nHost: www.httpbin.org\r\nContent-Length: 3\r\n\r\nb=1'
-s.send("3.85.154.144", 80, ddd)
 
+s.get("http://www.httpbin.org/get")
+# s.get("http://www.httpbin.org/get")
